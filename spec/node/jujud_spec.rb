ea988911846eedfaa0a44044151a2e36f67efbd7
Nobuto MURATA <nobuto@ubuntu.com>
2014-06-01 04:59:59 +0900

    fix regex to avoid hitting to grep

--- spec/node/jujud_spec.rb
+++ spec/node/jujud_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe command('pgrep -f "jujud machine"') do
+describe command('pgrep -f "jujud machin[e]"') do
   it { should return_exit_status 0 }
 end
 
-describe command('pgrep -f "jujud unit"') do
+describe command('pgrep -f "jujud uni[t]"') do
   it { should return_exit_status 0 }
 end
